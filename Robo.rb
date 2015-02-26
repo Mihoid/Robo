098558c45334dd0b4977c9e538543879bd879c09
michlen <mishlen-mishlen@mail.ru>
2015-02-26 13:04:52 +0300

    New stream

--- Robo.rb
+++ Robo.rb
@@ -1,36 +1,26 @@
-class Table
-  def data
-  dt = gets.to_i until (dt == 1 || dt == 2 || dt == 3 || dt == 4 || dt == 5)
+class Robo
+  def move
+	  x = x + 1 if ((1...5).include?(x)) || (vector == "West")
+	  x = x - 1 if ((1...5).include?(x)) || (vector == "East")
+	  y = y + 1 if ((1...5).include?(y)) || (vector == "North")
+	  y = y - 1 if ((1...5).include?(y)) || (vector == "South")
+  end
+
+  def right
+    vector == "West" if vector == "North"
+    vector == "South" if vector == "West"
+    vector == "East" if vector == "South"
+    vector == "North" if vector == "East"
+  end
+
+  def left
+	vector == "West" if vector == "South"
+    vector == "South" if vector == "East"
+    vector == "East" if vector == "North"
+    vector == "North" if vector == "West"	
   end
 end
-class Robo
 
-
-
-end
-
-x = Table.new
-y = Table.new
+x = Robo.new
+y = Robo.new
 vector = Robo.new
-maxstep = Robo.new
-
-print "Put x please (1-5). x="
-x.data
-print "Put y please (1-5). y="
-y = gets.to_i until (y == 1 || y == 2 || y ==3 || y == 4 || y == 5)
-puts "x=#{x}, y=#{y}"
-puts "puts your vector North, South, East, West"
-vector = gets.chomp.capitalize until (vector == "North" || vector =="South" || vector =="East" || vector =="West")
-puts "Your vector is #{vector}"
-if vector == "North"
-  maxstep = 5 - y 
-  elsif vector == "South"
-  maxstep = y - 1
-  elsif vector == "East"
-  maxstep = x - 1
-  elsif vector == "West"
-  maxstep = 5 - x
-end
-puts maxstep
-puts "Put forward or back for moving"
-move = gets.chomp.downcase until (move == "forward" || move == "back")
