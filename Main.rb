77e357f9e66b89c7588b7d41609921113be5a672
michlen <mishlen-mishlen@mail.ru>
2015-02-28 18:04:15 +0300

    Added gets X Y VECTOR

--- Main.rb
+++ Main.rb
@@ -1,22 +1,42 @@
 require_relative 'Robo'
 robo = Robo.new
-robo.place(0, 0, "North")
 
-puts "Now you can control your robo!Left, right, move!"
+
+
+puts "Enter X Y VECTOR to put robo on table"
+x, y, vector = str = gets.chomp.split
+
+until ((0..4).include?(x.to_i)) && ((0..4).include?(y.to_i)) && ((vector.capitalize == "North" || vector.capitalize == "South" || vector.capitalize == "East" || vector.capitalize == "West"))
+  str = gets.chomp
+  x, y, vector = str.split
+  puts x, y, vector
+end
+
+# Возможна ли такая конструкция для определения условий?
+# until (/0?,1?,2?,3?,4?/ === x) && (/0?,1?,2?,3?,4?/ === y) && (/North?,South?,East?,West?/ === vector)
+  # str = gets.chomp
+  # x, y, vector = str.split
+# end
+
+
+x = x.to_i
+y = y.to_i
+vector = vector.capitalize
+robo.place(x, y, vector)
+
+puts "Now you can control your robo!Left, right, move! or stop"
 word = 0
-until word == "breake"
-  (word = gets.chomp) && (
+until word == "stop"
+  (word = gets.chomp.downcase) && (
   case word.downcase
-    when "right" || RIGHT 
+    when "right"
 	robo.right
-	puts robo.inspect
+    puts robo.inspect
     when "left"
     robo.left
-	puts robo.inspect
+    puts robo.inspect
     when "move"
     robo.move
-	puts robo.inspect
-  else
-    puts "Please input correct command"
+    puts robo.inspect
   end)
 end
--- Robo.rb
+++ Robo.rb
@@ -1,43 +1,43 @@
 class Robo
 
-  def place(x, y, vector)
-  	@x = x
-	@y = y
-	@vector = vector
-  end
+def place(x, y, vector)
+  @x = x.to_i
+  @y = y.to_i
+  @vector = vector
+end
   
-  def move
-   	if (((1..4).include?(@x)) && (@vector == "West"))
-	  (@x = @x - 1)
-	elsif (((0...4).include?(@x)) && (@vector == "East"))
-	  (@x = @x + 1)
-	elsif (((0...4).include?(@y)) && (@vector == "North"))
-	  (@y = @y + 1)
-	elsif (((1..4).include?(@y)) && (@vector == "South"))
-	  (@y = @y - 1)
-	else
-	puts "It's impossible!"
-	end
+def move
+  if (((1..4).include?(@x)) && (@vector == "West"))
+    (@x = @x - 1)
+  elsif (((0...4).include?(@x)) && (@vector == "East"))
+    (@x = @x + 1)
+  elsif (((0...4).include?(@y)) && (@vector == "North"))
+    (@y = @y + 1)
+  elsif (((1..4).include?(@y)) && (@vector == "South"))
+    (@y = @y - 1)
+  else
+  puts "There is no way to move"
   end
+end
 
-  def right
-    @vector = case @vector	
-    when "North" then "East" 
-    when "West" then "North" 
-    when "South" then "West"
-    when "East" then "South"
-    end
+def right
+  @vector = case @vector	
+  when "North" then "East" 
+  when "West" then "North" 
+  when "South" then "West"
+  when "East" then "South"
   end
+end
   
 
-  def left
-    @vector = case @vector	
+def left
+  @vector = case @vector	
     when "North" then "West" 
     when "West" then "South" 
     when "South" then "East"
     when "East" then "North"
     end	
-  end
+end
   
 end
 
